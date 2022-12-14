bf727accec84ffb9c5ba87315a32861a22c509a7
Harald Sager Voehringer <voehring@login02.cluster.embl.de>
2022-12-14 15:54:37 +0100

    blacked code

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,8 @@ import numpy as np
 import pytest
 from scipy.sparse import csr_matrix
 
-@pytest.fixture(scope='module')
+
+@pytest.fixture(scope="module")
 def test_sparse_anndata():
     counts = csr_matrix(np.random.poisson(1, size=(100, 2000)), dtype=np.float32)
     adata = ad.AnnData(counts)
--- tests/test_data.py
+++ tests/test_data.py
@@ -4,6 +4,6 @@ from scCCA.data import get_rna_counts
 
 def test_get_rna_counts(test_sparse_anndata):
     X = get_rna_counts(test_sparse_anndata)
-    
+
     assert type(X) == np.ndarray
     assert X.shape == (100, 2000)
