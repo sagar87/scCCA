bac3017849d244a12be5d78287e64995948186c7
Harald Vohringer <harald.voeh@gmail.com>
2023-05-23 11:40:38 +0200

    parameterized tests for get_ordered_genes

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from scCCA.utils import get_ordered_genes, get_rna_counts
 
@@ -10,24 +11,33 @@ def test_get_rna_counts(test_sparse_anndata):
     assert X.shape == (100, 2000)
 
 
-def test_get_ordered_genes(test_anndata):
+@pytest.mark.parametrize(
+    "model_key, state, factor, highest, lowest, vector",
+    [
+        ("m2", "Intercept", 0, 50, 0, "W_rna"),
+        ("m2", "Intercept", 5, 50, 0, "W_rna"),
+        ("m2", "label[T.stim]", 0, 50, 0, "W_rna"),
+        ("m2", "Intercept", 0, 0, 50, "W_rna"),
+        ("m2", "Intercept", 5, 0, 50, "W_rna"),
+        ("m2", "label[T.stim]", 0, 0, 50, "W_rna"),
+    ],
+)
+def test_get_ordered_genes(model_key, state, factor, highest, lowest, vector, test_anndata):
     # test that the right order of genes is returned
-    model_key = "m2"
-    state = "Intercept"
-    factor = 0
-    highest = 50
-    lowest = 0
-    vector = "W_rna"
 
     df = get_ordered_genes(
         test_anndata, model_key=model_key, state=state, factor=factor, highest=highest, lowest=lowest, vector=vector
     )
 
     state_index = test_anndata.uns[model_key]["design"][state]
-    highest_index = np.argsort(test_anndata.varm[f"{model_key}_{vector}"][..., state_index, factor])[-highest:]
+    order = np.argsort(test_anndata.varm[f"{model_key}_{vector}"][..., factor, state_index])
 
-    assert df["gene"].tolist() == test_anndata.var_names[highest_index][::-1].tolist()
+    if highest == 0:
+        gene_idx = order[:lowest]
+    else:
+        gene_idx = np.concatenate([order[:lowest], order[-highest:]])
+
+    assert df["gene"].tolist() == test_anndata.var_names[gene_idx][::-1].tolist()
     assert np.all(
-        df["value"].to_numpy()
-        == test_anndata.varm[f"{model_key}_{vector}"][..., state_index, factor][highest_index][::-1]
+        df["value"].to_numpy() == test_anndata.varm[f"{model_key}_{vector}"][..., factor, state_index][gene_idx][::-1]
     )
