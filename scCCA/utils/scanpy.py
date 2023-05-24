6102aa84522a9109ccf75651c2b15fb54fa94aa6
Harald Vohringer <harald.voeh@gmail.com>
2023-05-24 09:32:14 +0200

    added docstrings to the convinience umap function

--- scCCA/utils/scanpy.py
+++ scCCA/utils/scanpy.py
@@ -47,12 +47,48 @@ def _get_model_design(adata: AnnData, model_key: str):
     model_dict = adata.uns[model_key]
 
     if DESIGN_KEY not in model_dict:
-        raise ValueError(f"No design mapping found in model annotations.")
+        raise ValueError("No design mapping found in model annotations.")
 
     return model_dict[DESIGN_KEY]
 
 
-def umap(adata, basis, neighbors_kwargs={}, umap_kwargs={}):
+def umap(adata: AnnData, basis: str, neighbors_kwargs: dict = {}, umap_kwargs: dict = {}):
+    """
+    Performs UMAP dimensionality reduction on an AnnData object.
+
+    Parameters
+    ----------
+    adata : AnnData
+        The AnnData object containing the data to be processed.
+    basis : str
+        The basis to use for the UMAP calculation.
+    neighbors_kwargs : dict, optional
+        Additional keyword arguments to be passed to `sc.pp.neighbors` function.
+        Default is an empty dictionary.
+    umap_kwargs : dict, optional
+        Additional keyword arguments to be passed to `sc.tl.umap` function.
+        Default is an empty dictionary.
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    This function performs UMAP dimensionality reduction on the input `adata` object
+    using the specified `basis`. It first computes the neighbors graph using the
+    `sc.pp.neighbors` function, with the option to provide additional keyword arguments
+    via `neighbors_kwargs`. Then, it applies the UMAP algorithm using the `sc.tl.umap`
+    function, with the option to provide additional keyword arguments via `umap_kwargs`.
+    Finally, it stores the UMAP coordinates in the `obsm` attribute of the `adata` object
+    under the key `"{basis}_umap"`, and removes the original `"X_umap"` coordinates.
+
+    Example
+    -------
+    >>> adata = AnnData(X)
+    >>> umap(adata, basis="pca", neighbors_kwargs={"n_neighbors": 10}, umap_kwargs={"min_dist": 0.5})
+
+    """
     sc.pp.neighbors(adata, use_rep=f"{basis}", key_added=f"{basis}", **neighbors_kwargs)
     sc.tl.umap(adata, neighbors_key=f"{basis}", **umap_kwargs)
     adata.obsm[f"{basis}_umap"] = adata.obsm["X_umap"]
