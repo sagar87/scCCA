bd747888acb413a9ea36fdc0de112d4369f777d9
Harald Vohringer <harald.voeh@gmail.com>
2023-05-24 10:07:53 +0200

    added tests and docstrings for get_rna_counts

--- scCCA/utils/data.py
+++ scCCA/utils/data.py
@@ -67,20 +67,39 @@ def extract_counts(adata, layers_key, protein_obsm_key):
 
 def get_rna_counts(adata: AnnData, layers_key: Union[str, None] = None) -> np.ndarray:
     """
-    Extracts RNA counts from AnnData object.
+    Extracts RNA counts from an AnnData object.
 
     Parameters
     ----------
-    adata: AnnData
-        AnnData object.
-    layers_key: str, optional (default: None)
-        If layers_key is None, then the raw counts are extracted from adata.X.
-        Otherwise, the counts are extracted from adata.layers[layers_key].
+    adata : AnnData
+        Annotated data object containing RNA counts.
+    layers_key : str or None, optional (default: None)
+        Key to specify the layer from which to extract the counts.
+        If `None`, the raw counts are extracted from `adata.X`.
+        If a valid `layers_key` is provided, the counts are extracted from `adata.layers[layers_key]`.
 
     Returns
     -------
-    X: np.ndarray
-        RNA counts matrix.
+    X : np.ndarray
+        RNA counts matrix as a NumPy array.
+
+    Raises
+    ------
+    KeyError
+        If `layers_key` is provided and not found in `adata.layers`.
+
+    Notes
+    -----
+    - If `layers_key` is `None`, the function extracts the counts from the attribute `adata.X`,
+      which is assumed to contain the raw counts.
+    - If `layers_key` is provided, the function checks if it exists in `adata.layers`.
+      If found, the counts are extracted from `adata.layers[layers_key]`.
+      If not found, a `KeyError` is raised.
+
+    The function first checks if the counts are stored as a sparse matrix (`issparse(X)`).
+    If so, the sparse matrix is converted to a dense array using `X.toarray()`.
+
+    Finally, the counts are returned as a NumPy array with the data type set to `np.float32`.
     """
     if layers_key is None:
         X = adata.X
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 from anndata import AnnData
+from scipy.sparse import csr_matrix
 
 from scCCA.utils import get_diff_genes, get_ordered_genes, get_rna_counts
 from scCCA.utils.data import _get_model_design
@@ -197,3 +198,42 @@ def test_get_model_design_missing_design_mapping():
 
     with pytest.raises(ValueError):
         _get_model_design(adata, model_key)
+
+
+def test_get_rna_counts_with_X():
+    # Test when layers_key is None, extracting from adata.X
+    adata = AnnData(X=np.array([[1, 2, 3], [4, 5, 6]]))
+    expected = np.array([[1, 2, 3], [4, 5, 6]])
+    result = get_rna_counts(adata)
+    assert np.array_equal(result, expected)
+
+
+def test_get_rna_counts_with_layers():
+    # Test when layers_key is provided, extracting from adata.layers[layers_key]
+    adata = AnnData(X=np.array([[1, 2, 3], [4, 5, 6]]), layers={"counts": np.array([[7, 8, 9], [10, 11, 12]])})
+    expected = np.array([[7, 8, 9], [10, 11, 12]])
+    result = get_rna_counts(adata, layers_key="counts")
+    assert np.array_equal(result, expected)
+
+
+def test_get_rna_counts_with_sparse_matrix():
+    # Test when layers_key is provided, and X is a sparse matrix
+    adata = AnnData(X=csr_matrix([[1, 0, 3], [0, 5, 0]]), layers={"counts": csr_matrix([[7, 0, 9], [0, 11, 0]])})
+    expected = np.array([[7, 0, 9], [0, 11, 0]])
+    result = get_rna_counts(adata, layers_key="counts")
+    assert np.array_equal(result, expected)
+
+
+def test_get_rna_counts_return_type():
+    # Test the return type of the function
+    adata = AnnData(X=np.array([[1, 2, 3], [4, 5, 6]]))
+    result = get_rna_counts(adata)
+    assert isinstance(result, np.ndarray)
+    assert result.dtype == np.float32
+
+
+def test_get_rna_counts_with_invalid_layers_key():
+    # Test when invalid layers_key is provided
+    adata = AnnData(X=np.array([[1, 2, 3], [4, 5, 6]]), layers={"counts": np.array([[7, 8, 9], [10, 11, 12]])})
+    with pytest.raises(KeyError):
+        get_rna_counts(adata, layers_key="invalid_key")
