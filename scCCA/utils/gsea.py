4582ed806197905cca05d861d08714ae378ed5f1
Harald Vohringer <harald.voeh@gmail.com>
2023-05-25 10:16:05 +0200

    renamed argument in gsea.py functions

--- scCCA/utils/gsea.py
+++ scCCA/utils/gsea.py
@@ -2,14 +2,15 @@ from typing import List, Union
 
 import gseapy as gp
 import pandas as pd
+from anndata import AnnData
 
 from .design import get_diff_genes, get_ordered_genes
 
 
 def get_factor_enrichment(
-    adata,
+    adata: AnnData,
     model_key: str,
-    states: Union[str, List[str]],
+    state: str,
     factor: int,
     highest: int = 50,
     lowest: int = 0,
@@ -21,84 +22,50 @@ def get_factor_enrichment(
         raise ValueError("Plese provide either highest or lower by setting one of them to zero.")
 
     ordered_genes = get_ordered_genes(
-        adata, model_key=model_key, state=states, factor=factor, highest=highest, lowest=lowest
+        adata,
+        model_key=model_key,
+        state=state,
+        factor=factor,
+        highest=highest,
+        lowest=lowest,
+        ascending=True if lowest > 0 else False,
     )
     enr = gp.enrichr(gene_list=ordered_genes["gene"].tolist(), gene_sets=geneset, organism=organism, outdir=None)
     return enr.results
 
 
-def geneset_enrichment_diff(
-    adata,
-    model_key,
-    states,
-    factor=0,
-    highest=50,
-    lowest=0,
-    sign=1.0,
-    geneset=["GO_Biological_Process_2021"],
-    organism="human",
+def get_diff_enrichment(
+    adata: AnnData,
+    model_key: str,
+    states: List[str],
+    factor: int = 0,
+    highest: int = 50,
+    lowest: int = 0,
+    sign: Union[int, float] = 1.0,
+    geneset: str = "GO_Biological_Process_2021",
+    organism: str = "human",
 ):
     # assert (highest > 0 and lowest == 0) or (highest == 0 and lowest > 0), "Either highest or lowest must be greater zero but not both."
+    if (highest > 0) and (lowest > 0):
+        raise ValueError("Plese provide either highest or lower by setting one of them to zero.")
+    diff_genes = get_diff_genes(
+        adata,
+        model_key,
+        states=states,
+        factor=factor,
+        highest=highest,
+        lowest=lowest,
+        sign=sign,
+        ascending=True if lowest > 0 else False,
+    )
 
-    if (highest > 0 and lowest == 0) or (highest == 0 and lowest > 0):
-        df = get_diff_genes(
-            adata,
-            model_key,
-            state=states,
-            factor=factor,
-            highest=highest,
-            lowest=lowest,
-            sign=sign,
-            ascending=True if lowest > 0 else False,
-        )
+    enr = gp.enrichr(
+        gene_list=diff_genes["gene"].tolist(),  # or "./tests/data/gene_list.txt",
+        gene_sets=geneset,
+        organism=organism,  # don't forget to set organism to the one you desired! e.g. Yeast
+        outdir=None,  # don't write to disk
+    )
 
-        enr = gp.enrichr(
-            gene_list=df["gene"].tolist(),  # or "./tests/data/gene_list.txt",
-            gene_sets=geneset,
-            organism=organism,  # don't forget to set organism to the one you desired! e.g. Yeast
-            outdir=None,  # don't write to disk
-        )
-        enr.results["Regulation"] = "Down" if lowest > 0 else "Up"
-        res = enr.results
-    else:
-        df_up = get_diff_genes(
-            adata,
-            model_key,
-            state=states,
-            factor=factor,
-            highest=highest,
-            lowest=0,
-            sign=sign,
-            ascending=False,
-        )
-
-        enr_up = gp.enrichr(
-            gene_list=df_up["gene"].tolist(),  # or "./tests/data/gene_list.txt",
-            gene_sets=geneset,
-            organism=organism,  # don't forget to set organism to the one you desired! e.g. Yeast
-            outdir=None,  # don't write to disk
-        )
-
-        df_down = get_diff_genes(
-            adata,
-            model_key,
-            state=states,
-            factor=factor,
-            highest=0,
-            lowest=lowest,
-            sign=sign,
-            ascending=True,
-        )
-
-        enr_down = gp.enrichr(
-            gene_list=df_down["gene"].tolist(),  # or "./tests/data/gene_list.txt",
-            gene_sets=geneset,
-            organism=organism,  # don't forget to set organism to the one you desired! e.g. Yeast
-            outdir=None,  # don't write to disk
-        )
-
-        enr_up.results["Regulation"] = "Up"
-        enr_down.results["Regulation"] = "Down"
-        res = pd.concat([enr_up.results, enr_down.results])
-
+    enr.results["Regulation"] = "Down" if lowest > 0 else "Up"
+    res = enr.results
     return res
