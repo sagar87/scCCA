08c8d2370eaecc5c60a9cc82d78eaaa411489af1
Harald Vohringer <harald.voeh@gmail.com>
2023-05-30 10:22:45 +0200

    export loading_state and factor_enrichment functions

--- scCCA/plots/__init__.py
+++ scCCA/plots/__init__.py
@@ -1,13 +1,17 @@
 from .factor_embedding import factor_embedding
+from .factor_enrichment import factor_enrichment
 from .loadings_bar import loading_bar
 from .loadings_scatter import loadings_scatter
+from .loadings_state import loadings_state
 from .qc import disp, mean_var, qc_hist
 
 __all__ = [
     "disp",
     "qc_hist",
     "factor_embedding",
+    "factor_enrichment",
     "loading_bar",
     "loadings_scatter",
+    "loadings_state",
     "mean_var",
 ]
