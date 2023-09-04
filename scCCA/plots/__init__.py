79e40cc708720163c1c739db8336285607aa0291
Harald Vohringer <harald.voeh@gmail.com>
2023-09-04 10:29:11 +0200

    export of cluster_enrichment in the plotting module

--- scCCA/plots/__init__.py
+++ scCCA/plots/__init__.py
@@ -1,9 +1,10 @@
+from .cluster_enrichment import cluster_enrichment
 from .data_matrix import data_matrix
 from .design_matrix import design_matrix
 from .factor_embedding import factor_embedding
 from .factor_enrichment import factor_enrichment
 from .loadings_bar import loading_bar
-from .loadings_heatmap import loadings_heatmap
+from .loadings_heatmap import loadings_diff_heatmap, loadings_heatmap
 from .loadings_scatter import loadings_scatter
 from .loadings_state import loadings_state
 from .qc import disp, mean_var, qc_hist
@@ -11,11 +12,13 @@ from .qc import disp, mean_var, qc_hist
 __all__ = [
     "disp",
     "qc_hist",
+    "cluster_enrichment",
     "factor_embedding",
     "factor_enrichment",
     "loading_bar",
     "loadings_scatter",
     "loadings_state",
+    "loadings_diff_heatmap",
     "loadings_heatmap",
     "data_matrix",
     "design_matrix",
